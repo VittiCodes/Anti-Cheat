f19bd9cd5bb16967bf82753827b412d02ba863e9
João Silva <75593113+Z4Dev@users.noreply.github.com>
2022-09-18 17:09:51 +0100

    Update vitti-c.lua

--- [VC]Anti-Cheat/core/vitti-c.lua
+++ [VC]Anti-Cheat/core/vitti-c.lua
@@ -17,12 +17,15 @@ addEventHandler('onClientPreRender',getRootElement(), function()
         triggerServerEvent('Vitti:Detect',localPlayer,localPlayer,"JetPack-Hack")
     elseif getGameSpeed() > Config["Anti-Cheat"]["Speed"]["max-speed"] then
         triggerServerEvent('Vitti:Detect',localPlayer,localPlayer,"Speed-Hack")
-    elseif math.round(getGravity(),4) > Config["Anti-Cheat"]["Gravity"]["max-gravity"] then
+    elseif math.round(getGravity(),4) > Config["Anti-Cheat"]["Gravity"]["gravity"] or math.round(getGravity(),4) < Config["Anti-Cheat"]["Gravity"]["gravity"] then
         triggerServerEvent('Vitti:Detect',localPlayer,localPlayer,"Gravity-Hack")
     end
 end)
 
 addEventHandler('onClientExplosion',getRootElement(), function(_,_,_,type)
+    if type == 4 or type == 5 or type == 6 or type == 7 then
+        return;
+    end
     if isElement(source) and getElementType(source) == "player" then
         table.foreach(Config["Anti-Cheat"]["Explosion"]["explosion-ids"], function(index,value)
             if type == value then
